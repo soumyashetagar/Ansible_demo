8c2c3d665c46178a48c5af4b8a182e10dc61901d
revathisaraiswathi <revathisaraiswathi@gmail.com>
2019-10-18 01:47:35 -0700

    my commit

---
+++ src/main/test/WepApllication/LoginTest.java
@@ -0,0 +1,20 @@
+package WepApllication;
+
+import com.phoenix.Check;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class LoginTest {
+
+	@Test
+	void test() {
+		Check c=new Check();
+		String output = c.checking("admin", "admin");
+		assertEquals("success",output);
+	}
+
+}
